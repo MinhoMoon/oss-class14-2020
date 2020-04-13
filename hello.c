6afaa574501b83b33710de117e9669a9c5763a26
MinhoMoon <idmmm134@naver.com>
2020-04-13 13:38:03 +0900

    Add swap() function

--- hello.c
+++ hello.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+void swap(int* a, int* b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+	return;
+}
+
 int main()
 {
-	printf("hello\nWorld!!!!!");
+	printf("hello\nWorld!!!");
 	return 0;
 }
